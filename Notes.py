4703f921cd78ef22316f646905b4e75b3dcf2e06
GauthamHari000 <62849063+GauthamHari000@users.noreply.github.com>
2020-04-04 13:52:02 -0400

    Testing VC Integration

--- Notes.py
+++ Notes.py
@@ -2,3 +2,4 @@ userInput = input("Enter Notes Here:")
 userDoc = open("NotesTranscribedFile","w")
 userDoc.write(userInput)
 print("Your notes have been transcribed in the file 'NotesTranscribedFile'")
+#Testing VC Integration
