501f24b226b873e00867d9a59e81b7caf09f4fd8
GauthamHari000 <62849063+GauthamHari000@users.noreply.github.com>
2020-04-04 13:48:00 -0400

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,4 @@
+userInput = input("Enter Notes Here:")
+userDoc = open("NotesTranscribedFile","w")
+userDoc.write(userInput)
+print("Your notes have been transcribed in the file 'NotesTranscribedFile'")
